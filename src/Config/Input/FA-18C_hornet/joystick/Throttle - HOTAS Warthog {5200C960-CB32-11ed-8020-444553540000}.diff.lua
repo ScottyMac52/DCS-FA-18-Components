f24801932176a13f6a61b94c8cf13a95622412c5
Scott McIntosh <scottymac40love@gmail.com>
2026-09-08 16:58:43 -0700

    Checkpoint F/A-18C assignment changes before re-scaffolding

--- src/Config/Input/FA-18C_hornet/joystick/Throttle - HOTAS Warthog {5200C960-CB32-11ed-8020-444553540000}.diff.lua
+++ src/Config/Input/FA-18C_hornet/joystick/Throttle - HOTAS Warthog {5200C960-CB32-11ed-8020-444553540000}.diff.lua
@@ -1,278 +1,242 @@
--- TM Warthog Throttle - F/A-18C Hornet
--- The Warthog Throttle is a specific F/A-18C controller; functions are mapped
--- as closely as possible to the actual Hornet throttle layout.
--- Axis conflicts (JOY_Z/RZ default to thrust/rudder) are removed here.
 local diff = {
-	["keyDiffs"] = {
-		-- =========================================================
-		-- APU
-		-- =========================================================
-		["d3001pnilu3001cd12vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN20",
-				},
-			},
-			["name"] = "APU Control Switch - ON/OFF",
-		},
-		-- =========================================================
-		-- FLAP SWITCH (3-position)
-		-- =========================================================
-		["d3007pnilu3007cd2vd-1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN23",
-				},
-			},
-			["name"] = "FLAP 3-pos Switch: FULL<>HALF",
-		},
-		["d3007pnilu3007cd2vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN22",
-				},
-			},
-			["name"] = "FLAP 3-pos Switch: AUTO<>HALF",
-		},
-		-- =========================================================
-		-- MASTER CAUTION RESET
-		-- =========================================================
-		["d3008pnilu3008cd9vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN21",
-				},
-			},
-			["name"] = "MASTER CAUTION Reset Button",
-		},
-		-- =========================================================
-		-- HOTAS PADDLE / AP DISENGAGE (throttle-side)
-		-- =========================================================
-		["d3018pnilu3018cd13vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN26",
-				},
-			},
-			["name"] = "Autopilot/Nosewheel Steering Disengage (Paddle) Switch",
-		},
-		-- =========================================================
-		-- TDC (THROTTLE DESIGNATOR CONTROLLER)
-		-- =========================================================
-		["d3023pnilu3023cd13vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN1",
-				},
-			},
-			["name"] = "Throttle Designator Controller - DEPRESS",
-		},
-		-- =========================================================
-		-- RADAR ELEVATION CONTROL
-		-- =========================================================
-		["d3024pnilu3024cd13vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN_POV1_U",
-				},
-			},
-			["name"] = "Radar Elevation Control - Up",
-		},
-		["d3025pnilu3025cd13vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN_POV1_D",
-				},
-			},
-			["name"] = "Radar Elevation Control - Down",
-		},
-		-- =========================================================
-		-- COMM SWITCH
-		-- =========================================================
-		["d3027pnilu3027cd13vd0.2vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN3",
-				},
-			},
-			["name"] = "COMM Switch - COMM 1 (call radio menu)",
-		},
-		["d3028pnilu3028cd13vd0.4vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN5",
-				},
-			},
-			["name"] = "COMM Switch - COMM 2 (call radio menu)",
-		},
-		-- =========================================================
-		-- CAGE / UNCAGE (FOV)
-		-- =========================================================
-		["d3031pnilu3031cd13vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN12",
-				},
-			},
-			["name"] = "Cage/Uncage Button",
-		},
-		-- =========================================================
-		-- DISPENSE (CHAFF / FLARE)
-		-- =========================================================
-		["d3032pnilu3032cd13vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN9",
-				},
-			},
-			["name"] = "Dispense Switch - Forward (CHAFF) / Center (OFF)",
-		},
-		["d3033pnilu3033cd13vd-1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN10",
-				},
-			},
-			["name"] = "Dispense Switch - Aft (FLARE) / Center (OFF)",
-		},
-		-- =========================================================
-		-- RAID / FLIR FOV SELECT
-		-- =========================================================
-		["d3034pnilu3034cd13vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN11",
-				},
-			},
-			["name"] = "RAID/FLIR FOV Select Button",
-		},
-		-- =========================================================
-		-- SPEED BRAKE
-		-- =========================================================
-		["d3035pnilu3035cd13vd-1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN8",
-				},
-			},
-			["name"] = "Speed Brake Switch - EXTEND",
-		},
-		["d3035pnilunilcd13vd1vpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN7",
-				},
-			},
-			["name"] = "Speed Brake Switch - RETRACT",
-		},
-		-- =========================================================
-		-- ATC ENGAGE / DISENGAGE
-		-- =========================================================
-		["d3037pnilu3037cd13vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN15",
-				},
-			},
-			["name"] = "ATC Engage/Disengage Switch",
-		},
-		-- =========================================================
-		-- EXTERIOR LIGHTS
-		-- =========================================================
-		["d3041pnilunilcd13vd0vpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN14",
-				},
-			},
-			["name"] = "Exterior Lights Switch - OFF",
-		},
-		["d3041pnilunilcd13vd1vpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN13",
-				},
-			},
-			["name"] = "Exterior Lights Switch - ON",
-		},
-		-- =========================================================
-		-- HORNET BALL (carrier approach power system)
-		-- =========================================================
-		["d3041pnilunilcd40vd1vpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN2",
-				},
-			},
-			["name"] = "Hornet Ball",
-		},
-		-- =========================================================
-		-- Remove default view axis conflicts
-		-- =========================================================
-		["dnilp210u214cdnilvdnilvpnilvunil"] = {
-			["name"] = "View Up Right slow",
-			["removed"] = {
-				[1] = {
-					["key"] = "JOY_BTN_POV1_UR",
-				},
-			},
-		},
-		["dnilp211u214cdnilvdnilvpnilvunil"] = {
-			["name"] = "View Down Right slow",
-			["removed"] = {
-				[1] = {
-					["key"] = "JOY_BTN_POV1_DR",
-				},
-			},
-		},
-		["dnilp212u214cdnilvdnilvpnilvunil"] = {
-			["name"] = "View Down Left slow",
-			["removed"] = {
-				[1] = {
-					["key"] = "JOY_BTN_POV1_DL",
-				},
-			},
-		},
-		["dnilp213u214cdnilvdnilvpnilvunil"] = {
-			["name"] = "View Up Left slow",
-			["removed"] = {
-				[1] = {
-					["key"] = "JOY_BTN_POV1_UL",
-				},
-			},
-		},
-		["dnilp32u214cdnilvdnilvpnilvunil"] = {
-			["name"] = "View Left slow",
-			["removed"] = {
-				[1] = {
-					["key"] = "JOY_BTN_POV1_L",
-				},
-			},
-		},
-		["dnilp33u214cdnilvdnilvpnilvunil"] = {
-			["name"] = "View Right slow",
-			["removed"] = {
-				[1] = {
-					["key"] = "JOY_BTN_POV1_R",
-				},
-			},
-		},
-		["dnilp34u214cdnilvdnilvpnilvunil"] = {
-			["name"] = "View Up slow",
-			["removed"] = {
-				[1] = {
-					["key"] = "JOY_BTN_POV1_U",
-				},
-			},
-		},
-		["dnilp35u214cdnilvdnilvpnilvunil"] = {
-			["name"] = "View Down slow",
-			["removed"] = {
-				[1] = {
-					["key"] = "JOY_BTN_POV1_D",
-				},
-			},
-		},
-	},
+  ["keyDiffs"] = {
+    ["d3001pnilu3001cd12vd1vpnilvu0"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN20" },
+      },
+      ["name"] = "APU Control Switch - ON/OFF",
+    },
+    ["d3007pnilu3007cd2vd-1vpnilvu0"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN23" },
+      },
+      ["name"] = "FLAP 3-pos Switch: FULL<>HALF",
+    },
+    ["d3007pnilu3007cd2vd1vpnilvu0"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN22" },
+      },
+      ["name"] = "FLAP 3-pos Switch: AUTO<>HALF",
+    },
+    ["d3008pnilu3008cd9vd1vpnilvu0"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN21" },
+      },
+      ["name"] = "MASTER CAUTION Reset Button",
+    },
+    ["d3018pnilu3018cd13vd1vpnilvu0"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN26" },
+      },
+      ["name"] = "Autopilot/Nosewheel Steering Disengage (Paddle) Switch",
+    },
+    ["d3023pnilu3023cd13vd1vpnilvu0"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN1" },
+      },
+      ["name"] = "Throttle Designator Controller - DEPRESS",
+    },
+    ["d3024pnilu3024cd13vd1vpnilvu0"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN_POV1_U" },
+      },
+      ["name"] = "Radar Elevation Control - Up",
+    },
+    ["d3025pnilu3025cd13vd1vpnilvu0"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN_POV1_D" },
+      },
+      ["name"] = "Radar Elevation Control - Down",
+    },
+    ["d3027pnilu3027cd13vd0.2vpnilvu0"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN3" },
+      },
+      ["name"] = "COMM Switch - COMM 1 (call radio menu)",
+    },
+    ["d3028pnilu3028cd13vd0.4vpnilvu0"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN5" },
+      },
+      ["name"] = "COMM Switch - COMM 2 (call radio menu)",
+    },
+    ["d3031pnilu3031cd13vd1vpnilvu0"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN12" },
+      },
+      ["name"] = "Cage/Uncage Button",
+    },
+    ["d3032pnilu3032cd13vd1vpnilvu0"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN9" },
+      },
+      ["name"] = "Dispense Switch - Forward (CHAFF) / Center (OFF)",
+    },
+    ["d3033pnilu3033cd13vd-1vpnilvu0"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN10" },
+      },
+      ["name"] = "Dispense Switch - Aft (FLARE) / Center (OFF)",
+    },
+    ["d3034pnilu3034cd13vd1vpnilvu0"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN11" },
+      },
+      ["name"] = "RAID/FLIR FOV Select Button",
+    },
+    ["d3035pnilu3035cd13vd-1vpnilvu0"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN8" },
+      },
+      ["name"] = "Speed Brake Switch - EXTEND",
+    },
+    ["d3035pnilunilcd13vd1vpnilvunil"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN7" },
+      },
+      ["name"] = "Speed Brake Switch - RETRACT",
+    },
+    ["d3037pnilu3037cd13vd1vpnilvu0"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN15" },
+      },
+      ["name"] = "ATC Engage/Disengage Switch",
+    },
+    ["d3041pnilunilcd13vd0vpnilvunil"] = {
+      ["removed"] = {
+        [1] = { ["key"] = "JOY_BTN14" },
+      },
+      ["name"] = "Exterior Lights Switch - OFF",
+    },
+    ["d3041pnilunilcd13vd1vpnilvunil"] = {
+      ["removed"] = {
+        [1] = { ["key"] = "JOY_BTN13" },
+      },
+      ["name"] = "Exterior Lights Switch - ON",
+    },
+    ["d3041pnilunilcd40vd1vpnilvunil"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN2" },
+      },
+      ["name"] = "Hornet Ball",
+    },
+    ["dnilp210u214cdnilvdnilvpnilvunil"] = {
+      ["removed"] = {
+        [1] = { ["key"] = "JOY_BTN_POV1_UR" },
+      },
+      ["name"] = "View Up Right slow",
+    },
+    ["dnilp211u214cdnilvdnilvpnilvunil"] = {
+      ["removed"] = {
+        [1] = { ["key"] = "JOY_BTN_POV1_DR" },
+      },
+      ["name"] = "View Down Right slow",
+    },
+    ["dnilp212u214cdnilvdnilvpnilvunil"] = {
+      ["removed"] = {
+        [1] = { ["key"] = "JOY_BTN_POV1_DL" },
+      },
+      ["name"] = "View Down Left slow",
+    },
+    ["dnilp213u214cdnilvdnilvpnilvunil"] = {
+      ["removed"] = {
+        [1] = { ["key"] = "JOY_BTN_POV1_UL" },
+      },
+      ["name"] = "View Up Left slow",
+    },
+    ["dnilp32u214cdnilvdnilvpnilvunil"] = {
+      ["removed"] = {
+        [1] = { ["key"] = "JOY_BTN_POV1_L" },
+      },
+      ["name"] = "View Left slow",
+    },
+    ["dnilp33u214cdnilvdnilvpnilvunil"] = {
+      ["removed"] = {
+        [1] = { ["key"] = "JOY_BTN_POV1_R" },
+      },
+      ["name"] = "View Right slow",
+    },
+    ["dnilp34u214cdnilvdnilvpnilvunil"] = {
+      ["removed"] = {
+        [1] = { ["key"] = "JOY_BTN_POV1_U" },
+      },
+      ["name"] = "View Up slow",
+    },
+    ["dnilp35u214cdnilvdnilvpnilvunil"] = {
+      ["removed"] = {
+        [1] = { ["key"] = "JOY_BTN_POV1_D" },
+      },
+      ["name"] = "View Down slow",
+    },
+    ["d3001pnilunilcd71vd1vpnilvu0"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_POV1_D", ["reformers"] = { [1] = "MOZA_F16_F18_BTN3" } },
+      },
+      ["name"] = "Open DTC Interface",
+    },
+    ["d3014pnilunilcd23vd1vpnilvunil"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN24" },
+      },
+      ["name"] = "Master Arm Switch - ARM/SAFE",
+    },
+    ["d3015pnilu3015cd12vd-1vpnilvu0"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN31" },
+      },
+      ["name"] = "Engine Crank Switch - LEFT",
+    },
+    ["d3016pnilu3016cd12vd1vpnilvu0"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN32" },
+      },
+      ["name"] = "Engine Crank Switch - RIGHT",
+    },
+    ["d3016pnilunilcd12vd-1vpnilvunil"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN18" },
+        [2] = { ["key"] = "JOY_BTN19" },
+      },
+      ["name"] = "Engine Crank Switch - OFF",
+    },
+    ["d3028pnilunilcd3vd0vpnilvunil"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN16" },
+      },
+      ["name"] = "Left Generator Control Switch - NORM/OFF",
+    },
+    ["d3029pnilunilcd3vd0vpnilvunil"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN17" },
+      },
+      ["name"] = "Right Generator Control Switch - NORM/OFF",
+    },
+    ["d3003pnilu3003cd6vd1vpnilvu0"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_BTN17", ["reformers"] = { [1] = "MOZA_F16_F18_BTN3" } },
+      },
+      ["name"] = "Fuel Dump Switch - ON/OFF",
+    },
+  },
+  ["axisDiffs"] = {
+    ["a3044cd13"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_X" },
+      },
+      ["name"] = "Throttle Designator Controller - Horizontal Axis",
+    },
+    ["a3043cd13"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_Y" },
+      },
+      ["name"] = "Throttle Designator Controller - Vertical Axis",
+    },
+    ["a3023cd2"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_SLIDER1" },
+      },
+      ["name"] = "Throttles Friction Adjusting Lever",
+    },
+  },
 }
 return diff
